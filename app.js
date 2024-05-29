0e0c946a1fe09ffbbad19237fc65feeae0640935
Douglas Augusto <sunshinedn2003@gmail.com>
2024-05-29 08:03:37 -0300

    Middleware for rout not listed added

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.use(cors());
 app.use('/spreadsheet', indexRoute);
 app.use('/media', mediaRoute);
 
+app.use((req, res, next) => {
+  res.status(404).send('Page not found');
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
